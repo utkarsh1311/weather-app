ebd3578d1ad829c2ba30e9955e258fb67f84475a
Utkarsh <itsutkarsh123@gmail.com>
2022-06-07 21:17:19 +0530

    some changes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': 'url("https://images.pexels.com/photos/1424246/pexels-photo-1424246.jpeg")'
+      }
+    },
+  },
+  plugins: [],
+}
